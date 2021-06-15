161c33582757eb403fd5bfe2e43c0818d944fc42
việt đức <vietduc.uet@gmail.com>
2021-06-15 08:47:31 +0700

    add all

--- src/store/actions/auth.js
+++ src/store/actions/auth.js
@@ -1,7 +1,6 @@
 import {
   AUTHENTICATE,
   LOGIN_REQUEST,
-  LOGOUT_REQUEST,
   LOGIN_FAILED,
   LOGIN_SUCCESS,
   LOGOUT,
@@ -15,12 +14,18 @@ const loginSuccess = userId => {
     user: userId,
   };
 };
-export const loginRequest = (email, password) => {
+const loginFailed = text => {
   return {
-    type: LOGIN_REQUEST,
-    user: {email, password},
+    type: LOGIN_FAILED,
+    errorText: text,
   };
 };
+// export const loginRequest = (email, password) => {
+//   return {
+//     type: LOGIN_REQUEST,
+//     user: {email, password},
+//   };
+// };
 export const login = (email, password) => {
   return async dispatch => {
     dispatch({type: LOGIN_REQUEST});
@@ -33,7 +38,7 @@ export const login = (email, password) => {
       } else if (mess === 'INVALID_PASSWORD') {
         errorText = 'This password could be not found';
       }
-      dispatch({type: LOGIN_FAILED, errorText: errorText});
+      dispatch(loginFailed(errorText));
       return;
     }
     dispatch(loginSuccess(response.data.localId));
@@ -47,9 +52,9 @@ export const logout = () => {
   };
 };
 
-export const logoutRequest = () => {
-  return {type: LOGOUT_REQUEST};
-};
+// export const logoutRequest = () => {
+//   return {type: LOGOUT_REQUEST};
+// };
 
 export const authenticate = user => {
   return {type: AUTHENTICATE, user};
